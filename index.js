c2d52541b4225f5789a1a5f9bd4d27c0a6304192
Neesar Banna <Bannasqe@gmail.com>
2023-10-25 20:46:29 -0400

    reupload DOM modifying lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+newHeader.textContent = "John DOE is the champion of the West :)";
+
+
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
